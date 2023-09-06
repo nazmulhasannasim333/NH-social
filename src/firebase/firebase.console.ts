392c14b23d565b39930c29c30b4d13e24042265c
Md Nasim Hosen <mdnasimhosen333@gmail.com>
2023-09-07 01:55:29 +0600

    which post liked by current logged user

--- src/firebase/firebase.console.ts
+++ src/firebase/firebase.console.ts
@@ -1,9 +1,5 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_apiKey,
   authDomain: process.env.NEXT_PUBLIC_authDomain,
@@ -13,6 +9,5 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_appId,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export default app;
