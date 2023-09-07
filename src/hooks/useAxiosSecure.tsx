22fabc8567984f135d59da62a3753d94e7020655
Md Nasim Hosen <mdnasimhosen333@gmail.com>
2023-09-07 10:41:37 +0600

    axiossecure initiate

--- src/hooks/useAxiosSecure.tsx
+++ src/hooks/useAxiosSecure.tsx
@@ -4,6 +4,7 @@ import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 import { logoutUser } from "../firebase/firebaseAuth";
 
+// create base URL
 const axiosSecure = axios.create({
   baseURL: "https://nh-social-server.vercel.app",
 });
